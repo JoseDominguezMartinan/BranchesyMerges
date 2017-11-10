3b608d02947349f9e9e637af0acbc8c54a26353b
jdominguezmartinan <jdominguezmartinan@victoria1.danielcastelao.org>
2017-11-10 09:31:46 +0100

    c

---
+++ src/brachesymerges/ClaseRama2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brachesymerges;
+
+/**
+ *
+ * @author jdominguezmartinan
+ */
+public class ClaseRama2 {
+  static void proba() {
+    System.out.println("este exercicio é para facer probas , nuevo branch");
+}
+}
